f41d60a60cb714e4384c9a11b2b81c1e72050e35
SKYnSPACE <skynspace@kaist.ac.kr>
2022-12-20 15:05:51 +0900

    Initialize

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        sky: colors.sky,
+        teal: colors.teal,
+        cyan: colors.cyan,
+        rose: colors.rose,
+      },
+    },
+  },
+  plugins: [
+    // ...
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+  ],
+}
